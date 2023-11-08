93f216d43dd3d993234d20d2d3d3799bedd28fc4
abderrahman202 <81939084+abderrahman202@users.noreply.github.com>
2023-11-08 01:19:16 +0100

    update main.cpp

--- TD2/Ex2/main.cpp
+++ TD2/Ex2/main.cpp
@@ -1,25 +1,45 @@
-// Ex2_TD2.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+
 using namespace std;
+
 class vecteur3d {
 public:
-    float x=0, y=0, z=0;
-    float composents(float x1 = 0, float y1 = 0,float z1=0) {
-        x = x1;
-        y = y1;
-        z = z1;
+    float x1, y1, z1, x2, y2, z2, somme_x, somme_y, somme_z;
+
+    vecteur3d(float x_1 = 0, float y_1 = 0, float z_1 = 0, float x_2 = 0, float y_2 = 0, float z_2 = 0) {
+        x1 = x_1;
+        y1 = y_1;
+        z1 = z_1;
+
+        x2 = x_2;
+        y2 = y_2;
+        z2 = z_2;
     }
 
-    void display() {
-        cout <<"Le valeur du vecteur est :" << "(" << x << "," << y << "," << z << ")" << endl;
+    void display_composents() {
+        cout << "Le vecteur A est : (" << x1 << ", " << y1 << ", " << z1 << ")" << endl;
+        cout << "Le vecteur B est : (" << x2 << ", " << y2 << ", " << z2 << ")" << endl;
     }
+    somme(){
+        somme_x=x1+x2;
+        somme_y=y1+y2;
+        somme_z=z1+z2;
+    }
+    void GetValues(){
+
+
+    }
+    void display_somme(){
+        cout<<"La somme des vecteurs A et B est : "<<"("<<somme_x<<","<<somme_y<<","<<somme_z<<")"<<endl;
+    }
+
+
 };
 
-
-int main()
-{
-    vecteur3d v;
-    v.display();
+int main() {
+    vecteur3d v(1, 2, 3, 4, 5, 6); 
+    
+    v.display_composents();
+    v.display_somme();
+    return 0;
 }
